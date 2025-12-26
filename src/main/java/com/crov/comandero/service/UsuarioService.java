8ffe895a5425c8477c76a904ab88a131d100d05b
edancabrera <ecabrerait@gmail.com>
2025-12-26 13:39:28 -0700

    Creación de UsuarioService, dentro: el método login()

---
+++ src/main/java/com/crov/comandero/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package com.crov.comandero.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.crov.comandero.model.TipoCargo;
+import com.crov.comandero.model.Usuario;
+import com.crov.comandero.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario login(String claveComanda) {
+        Optional <Usuario> usuarioOpt = usuarioRepository.findByClaveComanda(claveComanda);
+        if(usuarioOpt.isPresent()) {
+            Usuario usuario = usuarioOpt.get();
+            if(Boolean.TRUE.equals(usuario.getActivo()) && (usuario.getTipoCargo() == TipoCargo.MESERO || usuario.getTipoCargo() == TipoCargo.ADMINISTRADOR)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+}
