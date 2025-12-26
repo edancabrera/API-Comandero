e10af3fc29ab1e65ad5bb65d7db638acb0646b36
edancabrera <ecabrerait@gmail.com>
2025-12-26 12:20:47 -0700

    commit inicial

---
+++ src/main/java/com/crov/comandero/ApiComanderoApplication.java
@@ -0,0 +1,13 @@
+package com.crov.comandero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComanderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComanderoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crov/comandero/ApiComanderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crov.comandero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComanderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
