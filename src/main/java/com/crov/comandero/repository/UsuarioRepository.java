35dfa7237199676f372891744a48f74c764aeb2f
edancabrera <ecabrerait@gmail.com>
2025-12-26 13:20:09 -0700

    Método personalizado findByClaveComanda

--- src/main/java/com/crov/comandero/repository/UsuarioRepository.java
+++ src/main/java/com/crov/comandero/repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package com.crov.comandero.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.crov.comandero.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+    Optional<Usuario> findByClaveComanda(String claveComanda);
 }
