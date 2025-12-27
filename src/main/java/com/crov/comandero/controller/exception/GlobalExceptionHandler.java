da20ba7ef34445ff9b240a315bc21457bad53765
edancabrera <ecabrerait@gmail.com>
2025-12-27 01:45:13 -0700

    Se creó el paquete exception dentro de controller/. Se añadió manejo centralizado de errores para las excepciones personalizadas creadas con anterioridad

---
+++ src/main/java/com/crov/comandero/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.crov.comandero.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.crov.comandero.service.exception.RolNoPermitidoException;
+import com.crov.comandero.service.exception.UsuarioInactivoException;
+import com.crov.comandero.service.exception.UsuarioNoEncontradoException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UsuarioNoEncontradoException.class)
+    public ResponseEntity<String> handleUsuarioNoEncontrado() {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("Clave inválida");
+    }
+
+    @ExceptionHandler(UsuarioInactivoException.class)
+    public ResponseEntity<String> handleUsuarioInactivo() {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("Usuario inactivo");
+    }
+
+    @ExceptionHandler(RolNoPermitidoException.class)
+    public ResponseEntity<String> handleRolNoPermitido() {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("Rol no autorizado");
+    }
+}
