032b099c7e3d8b3ae0586f1fceca63fcc80170f6
edancabrera <ecabrerait@gmail.com>
2025-12-28 14:21:57 -0700

    Service ProductoService creado en el paquete service. Método listarPrecioProductos() añadido en el Service

---
+++ src/main/java/com/crov/comandero/service/ProductoService.java
@@ -0,0 +1,30 @@
+package com.crov.comandero.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.crov.comandero.dto.ProductoPrecioDTO;
+import com.crov.comandero.model.Producto;
+import com.crov.comandero.repository.ProductoRepository;
+
+@Service
+public class ProductoService {
+    private final ProductoRepository productoRepository;
+
+    public ProductoService(ProductoRepository productoRepository){
+        this.productoRepository = productoRepository;
+    }
+
+    public List<ProductoPrecioDTO> listarPrecioProductos(){
+        List<Producto> productos = productoRepository.findAll();
+
+        return productos.stream()
+                .map(producto -> new ProductoPrecioDTO(
+                    producto.getIdProducto(),
+                    producto.getNombre(),
+                    producto.getPrecio1()
+                )).collect(Collectors.toList());
+    }
+}
