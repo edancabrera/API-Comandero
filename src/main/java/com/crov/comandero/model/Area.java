1aa7ef657ab0206ccf19b55c5b786099df880ae8
edancabrera <ecabrerait@gmail.com>
2025-12-28 15:03:41 -0700

    Entity Area creada en el paquete model

---
+++ src/main/java/com/crov/comandero/model/Area.java
@@ -0,0 +1,59 @@
+package com.crov.comandero.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "gv_areas")
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "numero_mesas")
+    private Integer numeroMesas;
+
+    @Column(name = "activo")
+    private Boolean activo;
+
+    @Column(name = "zona_fumadores")
+    private Boolean zonaFumadores;
+
+    public Area(){}
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Integer getNumeroMesas() {
+        return numeroMesas;
+    }
+    public void setNumeroMesas(Integer numeroMesas) {
+        this.numeroMesas = numeroMesas;
+    }
+    public Boolean getActivo() {
+        return activo;
+    }
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+    public Boolean getZonaFumadores() {
+        return zonaFumadores;
+    }
+    public void setZonaFumadores(Boolean zonaFumadores) {
+        this.zonaFumadores = zonaFumadores;
+    }
+
+}
