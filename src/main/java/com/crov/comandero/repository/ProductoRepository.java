07072ef022a0bc1cb09d06d606a717dfc0bcf656
edancabrera <ecabrerait@gmail.com>
2025-12-28 14:41:48 -0700

    Método personalizado findByActivo() añadido en ProductoRepository

--- src/main/java/com/crov/comandero/repository/ProductoRepository.java
+++ src/main/java/com/crov/comandero/repository/ProductoRepository.java
@@ -3,7 +3,9 @@ package com.crov.comandero.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crov.comandero.model.Producto;
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 
+    List<Producto> findByActivo(Boolean activo);
 }
