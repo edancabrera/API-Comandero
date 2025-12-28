5508ba30ef4c3a89d6cda9705c8a801f3246341c
edancabrera <ecabrerait@gmail.com>
2025-12-28 14:03:32 -0700

    Entity Producto creada en el paquete model

---
+++ src/main/java/com/crov/comandero/model/Producto.java
@@ -0,0 +1,88 @@
+package com.crov.comandero.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "gv_producto")
+public class Producto {
+    public Producto(){}
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "idproducto")
+    private Integer idProducto;
+
+    @Column(name = "cod_barras")
+    private String codBarras;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "costo")
+    private Double costo;
+
+    @Column(name = "activo")
+    private Boolean activo;
+
+    @Column(name = "precio1")
+    private Double precio1;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getCodBarras() {
+        return codBarras;
+    }
+
+    public void setCodBarras(String codBarras) {
+        this.codBarras = codBarras;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Double getPrecio1() {
+        return precio1;
+    }
+
+    public void setPrecio1(Double precio1) {
+        this.precio1 = precio1;
+    }
+}
