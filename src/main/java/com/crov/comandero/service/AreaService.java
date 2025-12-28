59abf5e807db3bab948973ab8c10233bd65fa81d
edancabrera <ecabrerait@gmail.com>
2025-12-28 15:13:33 -0700

    Service AreaService creado en el paquete service

---
+++ src/main/java/com/crov/comandero/service/AreaService.java
@@ -0,0 +1,21 @@
+package com.crov.comandero.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.crov.comandero.model.Area;
+import com.crov.comandero.repository.AreaRepository;
+
+@Service
+public class AreaService {
+    private final AreaRepository areaRepository;
+
+    public AreaService(AreaRepository areaRepository) {
+        this.areaRepository = areaRepository;
+    }
+
+    public List<Area> listarAreas(){
+        return areaRepository.findAll();
+    }
+}
