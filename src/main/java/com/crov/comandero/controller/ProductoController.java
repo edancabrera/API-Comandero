b2cbe687313a5f7c3b69b7d6d0b92e9d011c97b4
edancabrera <ecabrerait@gmail.com>
2025-12-28 14:26:58 -0700

    Controller ProductoController creado en el paquete controller. Endpoint para listar los productos y sus precios añadido al Controller

---
+++ src/main/java/com/crov/comandero/controller/ProductoController.java
@@ -0,0 +1,25 @@
+package com.crov.comandero.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.crov.comandero.dto.ProductoPrecioDTO;
+import com.crov.comandero.service.ProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class ProductoController {
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService){
+        this.productoService = productoService;
+    }
+
+    @GetMapping("/precios")
+    public ResponseEntity<List<ProductoPrecioDTO>> listarPrecioProductos() {
+        return ResponseEntity.ok(productoService.listarPrecioProductos());
+    }
+}
