8c6408bbce5acd944c07749d552246ffce6ed0e5
edancabrera <ecabrerait@gmail.com>
2025-12-28 15:07:06 -0700

    Repository AreaRepository en el paquete repository

---
+++ src/main/java/com/crov/comandero/repository/AreaRepository.java
@@ -0,0 +1,9 @@
+package com.crov.comandero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crov.comandero.model.Area;
+
+public interface AreaRepository extends JpaRepository<Area, Integer>{
+
+}
