43e4debabbd3ff1c2fb80a3d51553b1c31c1be32
edancabrera <ecabrerait@gmail.com>
2025-12-31 12:56:00 -0700

    Refactorización, findMenusUnicos() recupera sólo aquellos activos

--- src/main/java/com/crov/comandero/repository/CategoriaPlatilloRepository.java
+++ src/main/java/com/crov/comandero/repository/CategoriaPlatilloRepository.java
@@ -9,7 +9,7 @@ import com.crov.comandero.model.CategoriaPlatillo;
 import com.crov.comandero.model.Menu;
 
 public interface CategoriaPlatilloRepository extends JpaRepository<CategoriaPlatillo, Integer>{
-    @Query("SELECT DISTINCT c.menu FROM CategoriaPlatillo c")
+    @Query("SELECT DISTINCT c.menu FROM CategoriaPlatillo c WHERE c.activo = true")
     List<Menu> findMenusUnicos();
 
     List<CategoriaPlatillo> findByMenu(Menu menu);
