4ffa988555534a8bf499a01c5eb26294ab97a6bd
edancabrera <ecabrerait@gmail.com>
2025-12-31 11:11:14 -0700

    CategoriaPlatilloRepository creado en el paquete repository

---
+++ src/main/java/com/crov/comandero/repository/CategoriaPlatilloRepository.java
@@ -0,0 +1,9 @@
+package com.crov.comandero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crov.comandero.model.CategoriaPlatillo;
+
+public interface CategoriaPlatilloRepository extends JpaRepository<CategoriaPlatillo, Integer>{
+
+}
