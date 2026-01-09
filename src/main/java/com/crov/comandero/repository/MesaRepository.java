896d3e37b8f51e587f1b20af0cfdf3f81ca3824f
edancabrera <ecabrerait@gmail.com>
2026-01-09 12:53:59 -0700

    Implementación: método que recupera las mesas activas de acuerdo a su id de area 'findByAreaIdAndActivoTrue()' creado en MesaRepository

--- src/main/java/com/crov/comandero/repository/MesaRepository.java
+++ src/main/java/com/crov/comandero/repository/MesaRepository.java
@@ -3,6 +3,8 @@ package com.crov.comandero.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.crov.comandero.model.Mesa;
 
-public interface MesaRepository extends JpaRepository<Mesa, Integer>{
+import java.util.List;
 
+public interface MesaRepository extends JpaRepository<Mesa, Integer>{
+    List<Mesa> findByAreaIdAndActivoTrue(Integer areaid);
 }
