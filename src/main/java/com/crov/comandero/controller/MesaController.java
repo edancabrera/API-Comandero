a5fb10bf8f8ef6b28e1d2fd61092503c9d80bd22
edancabrera <ecabrerait@gmail.com>
2026-01-09 12:58:23 -0700

    Endpoint '/areas/{id}/mesas' expuesto en MesaController.

--- src/main/java/com/crov/comandero/controller/MesaController.java
+++ src/main/java/com/crov/comandero/controller/MesaController.java
@@ -1,13 +1,27 @@
 package com.crov.comandero.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-//import com.crov.comandero.service.MesaService;
+
+import com.crov.comandero.model.Mesa;
+import com.crov.comandero.service.MesaService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class MesaController {
-    // private final MesaService mesaService;
+    private final MesaService mesaService;
 
-    // public MesaController(MesaService mesaService) {
-    //     this.mesaService = mesaService;
-    // }
+    public MesaController(MesaService mesaService) {
+        this.mesaService = mesaService;
+    }
+
+    @GetMapping("/areas/{id}/mesas")
+    public ResponseEntity<List<Mesa>> listarMesasPorArea(@PathVariable Integer id) {
+        return ResponseEntity.ok(mesaService.obtenerMesasPorArea(id));
+    }
 }
