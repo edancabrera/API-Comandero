4160103f9af4e849f58b7bfaa1c9b3702f6ec4c4
edancabrera <ecabrerait@gmail.com>
2026-01-09 12:56:02 -0700

    Implementación: MesaService utiliza el método del Repository

--- src/main/java/com/crov/comandero/service/MesaService.java
+++ src/main/java/com/crov/comandero/service/MesaService.java
@@ -1,13 +1,21 @@
 package com.crov.comandero.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
-//import com.crov.comandero.repository.MesaRepository;
+
+import com.crov.comandero.model.Mesa;
+import com.crov.comandero.repository.MesaRepository;
 
 @Service
 public class MesaService {
-    // private final MesaRepository mesaRepository;
+    private final MesaRepository mesaRepository;
 
-    // public MesaService(MesaRepository mesaRepository){
-    //     this.mesaRepository = mesaRepository;
-    // }
+    public MesaService(MesaRepository mesaRepository){
+        this.mesaRepository = mesaRepository;
+    }
+
+    public List<Mesa> obtenerMesasPorArea(Integer idArea){
+        return mesaRepository.findByAreaIdAndActivoTrue(idArea);
+    }
 }
