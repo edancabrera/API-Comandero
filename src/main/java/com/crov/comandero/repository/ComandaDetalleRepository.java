01dfa2ac5d869db546ec696186ba637a35a4f718
edancabrera <ecabrerait@gmail.com>
2026-01-15 15:01:11 -0700

    Repositorys creados para Comanda y ComandeDetalle

---
+++ src/main/java/com/crov/comandero/repository/ComandaDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.crov.comandero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crov.comandero.model.ComandaDetalle;
+
+public interface ComandaDetalleRepository extends JpaRepository<ComandaDetalle, Integer>{
+
+}
---
+++ src/main/java/com/crov/comandero/repository/ComandaRepository.java
@@ -0,0 +1,9 @@
+package com.crov.comandero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crov.comandero.model.Comanda;
+
+public interface ComandaRepository extends JpaRepository<Comanda, Integer>{
+
+}
