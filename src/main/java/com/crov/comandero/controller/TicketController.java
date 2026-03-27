8b2af161a60952b4cd63c6b32841925f44bb4514
edancabrera <ecabrerait@gmail.com>
2026-03-27 12:00:58 -0700

    Se eliminó un import innecesario

--- src/main/java/com/crov/comandero/controller/TicketController.java
+++ src/main/java/com/crov/comandero/controller/TicketController.java
@@ -1,7 +1,6 @@
 package com.crov.comandero.controller;
 
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
