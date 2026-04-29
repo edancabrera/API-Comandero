463e5a4f68e17baf513f57a31986bf39bc05cb02
edancabrera <ecabrerait@gmail.com>
2026-04-29 11:09:14 -0700

    Adición de comandos ESC/POS en el método print() de la clase PrinterService para: avance y corte automático del papel del ticket

--- src/main/java/com/crov/comandero/service/PrinterService.java
+++ src/main/java/com/crov/comandero/service/PrinterService.java
@@ -21,11 +21,29 @@ public class PrinterService {
             throw new RuntimeException("No se encontró la impresora: " + printerName);
         }
 
-        byte[] bytes = content.getBytes(StandardCharsets.UTF_8);
+        //Conversión del contenido del ticket a bytes (UTF-8)
+        byte[] textBytes = content.getBytes(StandardCharsets.UTF_8);
 
+        //Comandos ESC/POS
+        //Avance de papel, (0X0A = LF (Line Feed))
+        byte[] feed = new byte[]{0x0A, 0x0A, 0x0A};
+        //Corte de papel (corte completo)
+        byte[] cut = new byte[]{0x1D, 0x56, 0x00};
+        //Construcción del mensaje final,
+        //Se contstruye un solo arreglo de bytes con [contenido del ticket][avance][corte]
+        byte[] finalBytes = new byte[textBytes.length + feed.length + cut.length];
+
+        //Copiar contenido del ticket
+        System.arraycopy(textBytes, 0, finalBytes, 0, textBytes.length);
+        //Agregar avance de papel después del contenido
+        System.arraycopy(feed, 0, finalBytes, textBytes.length, feed.length);
+        //Agregar comando de corte final
+        System.arraycopy(cut, 0, finalBytes, textBytes.length + feed.length, cut.length);
+
+        //Envio a Impresora
         DocPrintJob job = printer.createPrintJob();
         DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
-        Doc doc = new SimpleDoc(bytes, flavor, null);
+        Doc doc = new SimpleDoc(finalBytes, flavor, null);
 
         job.print(doc, null);
     }
